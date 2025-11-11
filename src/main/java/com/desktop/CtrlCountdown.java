6d056f684d4d79eefe04c35914a4c4107f3981a2
CarlosMG06 <220267058+CarlosMG06@users.noreply.github.com>
2025-11-11 18:06:59 +0100

    Main, Controllers de vistes (buits)

---
+++ src/main/java/com/desktop/CtrlCountdown.java
@@ -0,0 +1,5 @@
+package com.desktop;
+
+public class CtrlCountdown {
+
+}
---
+++ src/main/java/com/desktop/CtrlWaiting.java
@@ -0,0 +1,18 @@
+package com.desktop;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.json.JSONObject;
+
+import javafx.fxml.Initializable;
+
+public class CtrlWaiting implements Initializable {
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    }
+
+    public void receiveMessage(JSONObject messageObj) {
+    }
+}
