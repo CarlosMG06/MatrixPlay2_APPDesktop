5871e183fc007352a0157965f96b448fe4371578
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-11-12 17:28:42 +0100

    .

--- src/main/java/com/desktop/Main.java
+++ src/main/java/com/desktop/Main.java
@@ -101,13 +101,10 @@ public class Main extends Application {
         pauseDuring(1500, () -> { // Give time to show connecting message ...
 
             String host = ctrlConfig.txtHost.getText();
-            if (host == "localhost") {
-                protocol = "ws";
-                port = 3000;
-            } else {
-                protocol = "wss";
-                port = 80;
-            }
+            protocol = "wss";  // siempre ws
+            port = 443;       // siempre puerto 3000
+
+
             wsClient = UtilsWS.getSharedInstance(protocol + "://" + host + ":" + port);
     
             wsClient.onMessage((response) -> { Platform.runLater(() -> { wsMessage(response); }); });
