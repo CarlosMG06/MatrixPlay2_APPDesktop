9faff6c4423802b6d651ede7aaddcd0703ec0e51
CarlosMG06 <220267058+CarlosMG06@users.noreply.github.com>
2025-11-18 14:17:23 +0100

    rehecho gestión de WS y mensajes

--- src/main/java/com/desktop/CtrlWaiting.java
+++ src/main/java/com/desktop/CtrlWaiting.java
@@ -7,15 +7,18 @@ import org.json.JSONObject;
 
 import javafx.fxml.Initializable;
 
-public class CtrlWaiting implements Initializable {
+public class CtrlWaiting implements Initializable, Messages, MessageListener {
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+        JSONObject msgObj = new JSONObject();
+        msgObj.put("type", C_AWAITING_COUNTDOWN);
+        WSManager.client.safeSend(msgObj.toString());
     }
 
-    public void receiveMessage(JSONObject messageObj) {
-        String type = messageObj.optString("type", "");
-        if (type.equals("countdown")) {
+    public void receiveMessage(JSONObject msgObj) {
+        String type = msgObj.optString("type", "");
+        if (type.equals(T_COUNTDOWN)) {
             UtilsViews.setView("ViewCountdown");
         }
     }
