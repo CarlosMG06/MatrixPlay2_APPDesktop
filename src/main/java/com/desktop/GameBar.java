1a476db7cd304ebc59744c6eed43ca7fa6396afa
DenisFV05 <152627686+DenisFV05@users.noreply.github.com>
2025-11-21 16:08:31 +0100

    Squashed commit of the following:
    commit 27e2edf9e2faf9dec9a3c979c6a9236e1bf94f8a
    Author: CarlosMG06 <220267058+CarlosMG06@users.noreply.github.com>
    Date:   Thu Nov 20 17:44:26 2025 +0100
    
        revertit canvi de onSizeChanged
    
    commit 514786983883bac81460d1393e1497fe8a1e4db3
    Author: CarlosMG06 <220267058+CarlosMG06@users.noreply.github.com>
    Date:   Thu Nov 20 17:39:48 2025 +0100
    
        animación fluida de barra
    
        - flechas cambian la dirección de la barra en vez de moverla directamente
        - optimización de onSizeChanged y setDatos

--- src/main/java/com/desktop/GameBar.java
+++ src/main/java/com/desktop/GameBar.java
@@ -2,28 +2,29 @@ package com.desktop;
 
 public class GameBar {
     private double posX;
-    private int posY;
+    private double posY;
     private int width;
     private int height;
+    public static enum Direction {UP, STILL, DOWN};
+    private Direction dir;
 
-    public GameBar(double posX, int posY, int WIDTH, int height) {
+    public GameBar(double posX, int posY, int WIDTH, int height, Direction dir) {
         this.posX = posX;
         this.posY = posY;
         this.width = WIDTH;
         this.height = height;
+        this.dir = dir;
     }
-
-    public void setGameBar(double posX, int posY) {
-        this.posX = posX;
-        this.posY = posY;
-    }
-
+    
     public double getPosX() {
         return posX;
     }
 
-    public int getPosY() {
+    public double getPosY() {
         return posY;
+    }
+    public void setPosY(double posY) {
+        this.posY = posY;
     }
 
     public int getHeight() {
@@ -36,6 +37,19 @@ public class GameBar {
 
     public int getWidth() {
         return width;
+    }
+
+    public Direction getDirection() {
+        return dir;
+    }
+    public void setDirUp() {
+        dir = Direction.UP;
+    }
+    public void setDirDown() {
+        dir = Direction.DOWN;
+    }
+    public void setDirStill() {
+        dir = Direction.STILL;
     }
 
     public int moveUp() {
